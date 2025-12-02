22e8fdbe5a83664d2e339eafc977cd346d93c88f
Altay <altayoener@gmail.com>
2025-12-02 03:01:09 +0100

    Anpassung_ssl_disabled

--- backend/app/api/main.py
+++ backend/app/api/main.py
@@ -6,15 +6,17 @@ app = FastAPI()
 
 def get_db():
     return mysql.connector.connect(
-        host="localhost",
+        host="192.168.134.130",
         user="alt",
         password="admin",
-        database="DATAMART_PRD"
-        ssl_disabled=True
+        database="DATAMART_PRD",
+        ssl_ca=None,
+        ssl_verify_cert=False,
+        ssl_verify_identity=False
     )
 
 @app.get("/hot-offers")
-def get_hot_offers( limit: int = 10):
+def get_hot_offers(limit: int = 10):
     db = get_db()
     cursor = db.cursor(dictionary=True)
     cursor.execute(get_offers(), (limit,))
@@ -22,4 +24,3 @@ def get_hot_offers( limit: int = 10):
     cursor.close()
     db.close()
     return result
-
