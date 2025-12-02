5c05903b450c58222cc18b1678c2c420f44ed404
Altay <altayoener@gmail.com>
2025-12-02 04:03:57 +0100

    .

--- backend/app/api/main.py
+++ backend/app/api/main.py
@@ -1,16 +1,30 @@
 from fastapi import FastAPI
 from db.queries import get_offers
 import mysql.connector
-import MySQLdb
+import ssl
 
 app = FastAPI()
 
+# Erstelle ein SSLContext-Objekt, wenn du SSL verwenden möchtest
+context = ssl.create_default_context(ssl.Purpose.CLIENT_AUTH)
+
+# Optional: Lade Zertifikate (falls erforderlich)
+#context.load_cert_chain(certfile='/path/to/client-cert.pem', keyfile='/path/to/client-key.pem')
+#context.load_verify_locations(cafile='/path/to/ca-cert.pem')
+
+# Funktion, um die MySQL-Datenbankverbindung zu erstellen
 def get_db():
-    return MySQLdb.connect(
-        host="192.168.134.130",  # Die IP-Adresse des MySQL-Servers
+    # Verbindung zu MySQL mit SSL-Zertifikaten
+    return mysql.connector.connect(
+        host="localhost",
         user="alt",
         password="admin",
         database="DATAMART_PRD"
+        #ssl_ca="/path/to/ca-cert.pem",        # Das CA-Zertifikat
+        #ssl_cert="/path/to/client-cert.pem",  # Dein Client-Zertifikat
+        #ssl_key="/path/to/client-key.pem",    # Dein Client-Schlüssel
+        #ssl_verify_cert=True,                 # SSL-Zertifikat verifizieren
+        #ssl_verify_identity=True             # Identität des Servers verifizieren
     )
 
 @app.get("/hot-offers")
